3fed9eea69927733be984f0fa9d0090bc65f7866
Tulie Lab <28954951+tung68nt@users.noreply.github.com>
2026-03-10 14:40:16 +0700

    sync src/components/ui/progress.tsx

---
+++ src/components/ui/progress.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import * as React from "react"
+import * as ProgressPrimitive from "@radix-ui/react-progress"
+
+import { cn } from "@/lib/utils"
+
+function Progress({
+  className,
+  value,
+  size = "default",
+  color = "default",
+  ...props
+}: React.ComponentProps<typeof ProgressPrimitive.Root> & {
+  size?: "default" | "xs" | "sm" | "lg"
+  color?: "default" | "primary" | "success"
+}) {
+  return (
+    <ProgressPrimitive.Root
+      data-slot="progress"
+      className={cn(
+        "bg-muted relative h-2 w-full overflow-hidden rounded-full",
+        size === "xs" && "h-1",
+        size === "sm" && "h-1.5",
+        size === "lg" && "h-3",
+        className
+      )}
+      {...props}
+    >
+      <ProgressPrimitive.Indicator
+        data-slot="progress-indicator"
+        className={cn(
+          "bg-primary h-full w-full flex-1 transition-all",
+          color === "success" && "bg-primary"
+        )}
+        style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+      />
+    </ProgressPrimitive.Root>
+  )
+}
+
+export { Progress }
